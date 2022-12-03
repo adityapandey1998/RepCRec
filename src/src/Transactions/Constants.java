8c6d54b2157c65dcf550d743aafd040e203d85a2
Shubham Jha <jha1shubham@gmail.com>
2022-12-02 19:19:02 -0500

    Add Constants, Lock

---
+++ src/src/Transactions/Constants.java
@@ -0,0 +1,19 @@
+package Transactions;
+
+public class Constants {
+
+    public final static int NUM_SITES = 10;
+    public final static int NUM_VARIABLES = 20;
+
+    public enum TransactionType {
+        RW, RO
+    };
+
+    public enum OperationType {
+        READ, WRITE, COMMIT
+    };
+
+    public enum LockType {
+        UNLOCK, READ, WRITE
+    };
+}
