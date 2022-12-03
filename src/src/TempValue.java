62047981405c32986d13c37019128115dafb908a
Shubham Jha <jha1shubham@gmail.com>
2022-12-02 22:42:38 -0500

    Add TempValue vlass

---
+++ src/src/TempValue.java
@@ -0,0 +1,25 @@
+public class TempValue {
+    int value;
+    String transactionId;
+
+    public TempValue(int value, String transactionId) {
+        this.value = value;
+        this.transactionId = transactionId;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+}
