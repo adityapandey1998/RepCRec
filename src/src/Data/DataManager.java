8a0a3a8057d6a65db1922bcb63ccc51e4cd6be07
Shubham Jha <jha1shubham@gmail.com>
2022-12-03 17:17:38 -0500

    Restore dump comparator

--- src/src/Data/DataManager.java
+++ src/src/Data/DataManager.java
@@ -3,12 +3,8 @@ package Data;
 import static Transactions.Constants.NUM_VARIABLES;
 
 import Transactions.Constants;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+
+import java.util.*;
 
 public class DataManager {
 
@@ -173,11 +169,20 @@ public class DataManager {
     var.proposedValue = new ProposedValue(value, transactionId);
   }
 
+  class variableComparator implements Comparator<Variable> {
+    @Override
+    public int compare(Variable a, Variable b) {
+      return Integer.parseInt(a.variableId.substring(1)) - Integer.parseInt(b.variableId.substring(1));
+    }
+  }
+
   public void dump() {
-    String siteStatus = this.isUp ? "UP" : "DOWN";
+    String siteStatus = this.isUp ? "Up" : "Down";
     StringBuilder result = new StringBuilder(
-        String.format("site %d [%s] - ", this.siteId, siteStatus));
-    for (Variable v : this.dataMap.values()) {
+            String.format("Site %d (Status: %s) - ", this.siteId, siteStatus));
+    List<Variable> variableList = new ArrayList<>(this.dataMap.values());
+    Collections.sort(variableList, new variableComparator());
+    for (Variable v : variableList) {
       String varStr = String.format("%s: %d, ", v.variableId,
           v.getMostRecentlyCommittedValue().getValue());
       result.append(varStr);
