8b6e36a675995503c864605fd60ed94082483f00
Aditya Pandey <pandeyan98@gmail.com>
2022-12-03 00:00:45 -0500

    Pushing Newest Changed to Transaction Manager

--- src/src/Transactions/TransactionManager.java
+++ src/src/Transactions/TransactionManager.java
@@ -93,8 +93,11 @@ public class TransactionManager {
         String line = "";
         while ((line = reader.readLine()) != null) {
             if(handleDeadlock()) {
-
+                executeOp();
             }
+            executeInput(line);
+            executeOp();
+
         }
     }
 
@@ -148,15 +151,15 @@ public class TransactionManager {
         if (!transactionTable.containsKey(transactionId)) {
             System.out.println("Transaction "+ transactionId+ " doesn't exist in the transaction table!");
         } else {
+            int ts = transactionTable.get(transactionId).getStartTime();
             for (DataManager dataManager: dataManagerList) {
                 if (dataManager.isUp() && dataManager.hasVariable(variableId)) {
-                    boolean result = dataManager.readSnapshot(transactionId, variableId);
+                    boolean result = dataManager.readSnapshot(variableId, ts);
                     if (result) {
                         System.out.println(String.format("%s (RO) reads %s.%s: %d".format(
                                 transactionId, variableId, dataManager.getSiteId(), result.value)));
                         return true;
                     }
-
                 }
             }
         }
@@ -164,104 +167,198 @@ public class TransactionManager {
     }
 
     private boolean writeOp(String transactionId, String variableId, int value) {
-    }
-
-    public void processInputFile(String filePath) throws IOException {
-        int time=1;
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader(filePath));
-        } catch (FileNotFoundException fileNotFoundException) {
-            System.err.println("FileNotFoundException Raised");
-            return;
-        }
-        String line = "";
-        while((line = reader.readLine()) !=null) {
-//            Check Deadlock
-            if (line.startsWith("dump()")) {
-                dumpHandlers.dumpAction(siteList);
-            } else if (line.startsWith("beginRO")) {
-                String transactionId = line.substring(8, line.length() - 1);
-                Transaction transaction = new Transaction(transactionId, Constants.TransactionType.RO, time);
-                transactionList.add(transaction);
-            } else if (line.startsWith("begin")) {
-                String transactionId = line.substring(6, line.length() - 1);
-                Transaction transaction = new Transaction(transactionId, Constants.TransactionType.RW, time);
-                transactionList.add(transaction);
-            } else if (line.startsWith("R")) {
-                String inputLine = line.substring(2, line.length() - 1);
-                String[] inputLineSplit = inputLine.split(",");
-                String transactionId = inputLineSplit[0].trim();
-                String variableName = inputLineSplit[1].trim();
-
-                Transaction transactionToRead = null;
-                for (Transaction transaction : transactionList) {
-                    if (transaction.transactionId.equals(transactionId)) {
-                        transactionToRead = transaction;
-                        break;
-                    }
+        if (!transactionTable.containsKey(transactionId)) {
+            System.out.println("Transaction "+ transactionId+ " doesn't exist in the transaction table!");
+        } else {
+            boolean allSitesDown = true, canGetAllLocks = true;
+            for (DataManager dataManager : dataManagerList) {
+                if (dataManager.isUp() && dataManager.hasVariable(variableId)) {
+                    allSitesDown = false;
+                    boolean result = dataManager.getWriteLock(transactionId, variableId);
+                    if (!result)
+                        canGetAllLocks = false;
                 }
-
-            } else if (line.startsWith("W")) {
-                String inputLine = line.substring(2, line.length() - 1);
-                String[] inputLineSplit = inputLine.split(",");
-                String transactionId = inputLineSplit[0].trim();
-                String variableName = inputLineSplit[1].trim();
-                int value = Integer.parseInt(inputLineSplit[2].trim());
-
-                Transaction transactionToWrite = null;
-                for (Transaction transaction : transactionList) {
-                    if (transaction.transactionId.equals(transactionId)) {
-                        transactionToWrite = transaction;
-                        break;
-                    }
-                }
-            } else if (line.startsWith("fail")) {
-                int siteId = Integer.parseInt(line.substring(5, line.length() - 1).trim());
-                Site siteToFail = null;
-                for (Site site : siteList) {
-                    if (site.siteId == siteId) {
-                        siteToFail = site;
-                        OperationHandlers.siteHandlers.failSite(siteToFail, time);
-                        break;
-                    }
-                }
-                if (siteToFail==null) {
-                    System.out.println("Site doesn't exist");
-                }
-
-            } else if (line.startsWith("recover")) {
-                int siteId = Integer.parseInt(line.substring(8, line.length() - 1).trim());
-                Site siteToRecover = null;
-                for (Site site : siteList) {
-                    if (site.siteId == siteId) {
-                        siteToRecover = site;
-                        OperationHandlers.siteHandlers.recoverSite(siteToRecover, time);
-                        break;
-                    }
-                }
-                if (siteToRecover==null) {
-                    System.out.println("Site doesn't exist");
-                }
-            } else if (line.startsWith("end")) {
-                //endTransaction(op.substring(4, op.length() - 1));
-                String transactionId = line.substring(4, line.length() - 1).trim();
-                Transaction transactionToEnd = null;
-                for (Transaction transaction : transactionList) {
-                    if (transaction.transactionId.equals(transactionId)) {
-                        transactionToEnd = transaction;
-                        OperationHandlers.endHandler.endTransaction(transactionToEnd, siteList);
-                        break;
-                    }
-                }
-                if (transactionToEnd == null) {
-                    System.out.println("Nothing to end here!");
-                }
-//                endTransactionList.add(line.substring(4, line.length() - 1));
             }
+            if (!(canGetAllLocks && allSitesDown)) {
+                List<String> sitesWritten = new ArrayList<>();
+                for (DataManager dataManager : dataManagerList) {
+                    if (dataManager.isUp() && dataManager.hasVariable(variableId)) {
+                        dataManager.write(transactionId, variableId, value);
 
+                        Transaction transaction = transactionTable.get(transactionId)
+                        transaction.sitesAccessed.add(dataManager.getSiteId());
+                        transactionTable.put(transactionId, transaction);
 
+                        sitesWritten.add(dataManager.getSiteId());
+
+                    }
+                }
+                System.out.println(String.format("%s writes %s with value %s to sites %s",
+                        transactionId, variableId, value, sitesWritten));
+                return true;
+            }
         }
-
+        return false;
     }
+
+//    def process_instruction(self, command, args):
+//            """
+//        Process an instruction.
+//        If the instruction is Read or Write, add it to the operation queue.
+//        Otherwise, execute the instruction directly.
+//        :param command: "begin", "beginRO", "R", "W", "dump", "end", "fail",
+//         or "recover"
+//        :param args: list of arguments for a command
+//        """
+//            if command == "begin":
+//            self.begin(args[0])
+//    elif command == "beginRO":
+//            self.beginro(args[0])
+//    elif command == "R":
+//            self.add_read_operation(args[0], args[1])
+//    elif command == "W":
+//            self.add_write_operation(args[0], args[1], args[2])
+//    elif command == "dump":
+//            self.dump()
+//    elif command == "end":
+//            self.end(args[0])
+//    elif command == "fail":
+//            self.fail(int(args[0]))
+//    elif command == "recover":
+//            self.recover(int(args[0]))
+//            else:
+//    raise InvalidInstructionError("Unknown instruction")
+
+    private void dump() {
+        for (DataManager dataManager : dataManagerList) {
+            dataManager.dump();
+        }
+    }
+
+    private void executeInput(String line) {
+        if (line.startsWith("dump")) {
+            System.out.println("Dump:");
+            dump();
+        } else if (line.startsWith("fail")) {
+            int siteId = Integer.parseInt(line.substring(5, line.length() - 1).trim());
+            fail(siteId);
+        }
+    }
+
+    private void fail(int siteId) {
+        DataManager dataManager = dataManagerList.get(siteId);
+        if (dataManager.isUp()) {
+            System.out.println("Failing Site:" + siteId);
+            dataManager.fail(time);
+            for (Transaction transaction : transactionTable.values()) {
+                if (
+                        !(transaction.getTransactionType() == Constants.TransactionType.RO) &&
+                                (transaction.isLive) &&
+                                transaction.getSitesAccessed().contains(siteId)
+                ) {
+                    transaction.isLive = false;
+                }
+            }
+        } else {
+            System.out.println("Site " + siteId + " is down!!");
+        }
+    }
+//    public void processInputFile(String filePath) throws IOException {
+//        int time=1;
+//        BufferedReader reader;
+//        try {
+//            reader = new BufferedReader(new FileReader(filePath));
+//        } catch (FileNotFoundException fileNotFoundException) {
+//            System.err.println("FileNotFoundException Raised");
+//            return;
+//        }
+//        String line = "";
+//        while((line = reader.readLine()) !=null) {
+////            Check Deadlock
+//            if (line.startsWith("dump()")) {
+//                dumpHandlers.dumpAction(siteList);
+//            } else if (line.startsWith("beginRO")) {
+//                String transactionId = line.substring(8, line.length() - 1);
+//                Transaction transaction = new Transaction(transactionId, Constants.TransactionType.RO, time);
+//                transactionList.add(transaction);
+//            } else if (line.startsWith("begin")) {
+//                String transactionId = line.substring(6, line.length() - 1);
+//                Transaction transaction = new Transaction(transactionId, Constants.TransactionType.RW, time);
+//                transactionList.add(transaction);
+//            } else if (line.startsWith("R")) {
+//                String inputLine = line.substring(2, line.length() - 1);
+//                String[] inputLineSplit = inputLine.split(",");
+//                String transactionId = inputLineSplit[0].trim();
+//                String variableName = inputLineSplit[1].trim();
+//
+//                Transaction transactionToRead = null;
+//                for (Transaction transaction : transactionList) {
+//                    if (transaction.transactionId.equals(transactionId)) {
+//                        transactionToRead = transaction;
+//                        break;
+//                    }
+//                }
+//
+//            } else if (line.startsWith("W")) {
+//                String inputLine = line.substring(2, line.length() - 1);
+//                String[] inputLineSplit = inputLine.split(",");
+//                String transactionId = inputLineSplit[0].trim();
+//                String variableName = inputLineSplit[1].trim();
+//                int value = Integer.parseInt(inputLineSplit[2].trim());
+//
+//                Transaction transactionToWrite = null;
+//                for (Transaction transaction : transactionList) {
+//                    if (transaction.transactionId.equals(transactionId)) {
+//                        transactionToWrite = transaction;
+//                        break;
+//                    }
+//                }
+//            } else if (line.startsWith("fail")) {
+//                int siteId = Integer.parseInt(line.substring(5, line.length() - 1).trim());
+//                Site siteToFail = null;
+//                for (Site site : siteList) {
+//                    if (site.siteId == siteId) {
+//                        siteToFail = site;
+//                        OperationHandlers.siteHandlers.failSite(siteToFail, time);
+//                        break;
+//                    }
+//                }
+//                if (siteToFail==null) {
+//                    System.out.println("Site doesn't exist");
+//                }
+//
+//            } else if (line.startsWith("recover")) {
+//                int siteId = Integer.parseInt(line.substring(8, line.length() - 1).trim());
+//                Site siteToRecover = null;
+//                for (Site site : siteList) {
+//                    if (site.siteId == siteId) {
+//                        siteToRecover = site;
+//                        OperationHandlers.siteHandlers.recoverSite(siteToRecover, time);
+//                        break;
+//                    }
+//                }
+//                if (siteToRecover==null) {
+//                    System.out.println("Site doesn't exist");
+//                }
+//            } else if (line.startsWith("end")) {
+//                //endTransaction(op.substring(4, op.length() - 1));
+//                String transactionId = line.substring(4, line.length() - 1).trim();
+//                Transaction transactionToEnd = null;
+//                for (Transaction transaction : transactionList) {
+//                    if (transaction.transactionId.equals(transactionId)) {
+//                        transactionToEnd = transaction;
+//                        OperationHandlers.endHandler.endTransaction(transactionToEnd, siteList);
+//                        break;
+//                    }
+//                }
+//                if (transactionToEnd == null) {
+//                    System.out.println("Nothing to end here!");
+//                }
+////                endTransactionList.add(line.substring(4, line.length() - 1));
+//            }
+//
+//
+//        }
+//
+//    }
 }
