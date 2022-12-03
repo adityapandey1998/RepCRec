dff6fa1c30fb4be676207375392a65e2b28734aa
Shubham Jha <jha1shubham@gmail.com>
2022-12-02 22:44:34 -0500

    Add Result class

---
+++ src/src/Result.java
@@ -0,0 +1,25 @@
+public class Result {
+    boolean success;
+    int value;
+
+    public Result(boolean success, int value) {
+        this.success = success;
+        this.value = value;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
